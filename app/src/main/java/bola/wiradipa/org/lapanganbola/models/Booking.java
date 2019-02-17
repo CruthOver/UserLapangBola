3843790068ad3922ac343c447bf15311f9ee5dc2
Bambang Stiawan <b3ng.b3ng16@gmail.com>
2019-02-18 06:16:58 +0700

    Update Booking.java

--- app/src/main/java/bola/wiradipa/org/lapanganbola/models/Booking.java
+++ app/src/main/java/bola/wiradipa/org/lapanganbola/models/Booking.java
@@ -1,32 +1,29 @@
 package bola.wiradipa.org.lapanganbola.models;
 
-/**
- * Created by ikhsan ramadhan
- * =========================================
- * MyApplication
- * Copyright (C) 2/10/2019.
- * All rights reserved
- * -----------------------------------------
- * Name     : Muhamad Ikhsan Ramadhan
- * E-mail   : ikhsanramadhan28@gmail.com
- * Majors   : D3 Teknik Informatika 2016
- * Campus   : Telkom University
- * -----------------------------------------
- */
+import com.google.gson.annotations.SerializedName;
+
 public class Booking {
-    String jam, harga;
+    int status;
+    @SerializedName("tariff")
+    String harga;
+    @SerializedName("start_hour")
+    String mStartHour;
+    @SerializedName("end_hour")
+    String mEndHour;
+
+    public Booking() { }
 
     public Booking(String jam, String harga) {
-        this.jam = jam;
+        this.mStartHour = jam;
         this.harga = harga;
     }
 
     public String getJam() {
-        return jam;
+        return mStartHour;
     }
 
     public void setJam(String jam) {
-        this.jam = jam;
+        this.mStartHour = jam;
     }
 
     public String getHarga() {
@@ -35,5 +32,21 @@ public class Booking {
 
     public void setHarga(String harga) {
         this.harga = harga;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setEndHour(String mEndHour) {
+        this.mEndHour = mEndHour;
+    }
+
+    public String getEndHour() {
+        return mEndHour;
     }
 }
