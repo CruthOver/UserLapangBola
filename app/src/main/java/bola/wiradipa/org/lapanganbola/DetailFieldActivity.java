5c7e1f08a1d1b5ef6d28772f05a12d68cf4d8b42
ikhsanramadhaan <ikhsanramadhan28@gmail.com>
2019-02-22 20:17:01 +0700

    change gridview to recyclerview gridlayoutmanager

--- app/src/main/java/bola/wiradipa/org/lapanganbola/DetailFieldActivity.java
+++ app/src/main/java/bola/wiradipa/org/lapanganbola/DetailFieldActivity.java
@@ -10,8 +10,9 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
@@ -37,7 +38,6 @@ import java.util.List;
 import java.util.Locale;
 
 import bola.wiradipa.org.lapanganbola.controllers.BaseActivity;
-import bola.wiradipa.org.lapanganbola.helpers.AppSession;
 import bola.wiradipa.org.lapanganbola.helpers.DateHelper;
 import bola.wiradipa.org.lapanganbola.helpers.Log;
 import bola.wiradipa.org.lapanganbola.helpers.MoneyHelper;
@@ -45,9 +45,7 @@ import bola.wiradipa.org.lapanganbola.helpers.adapters.DoubleTypeAdapter;
 import bola.wiradipa.org.lapanganbola.helpers.adapters.FieldPagerAdapter;
 import bola.wiradipa.org.lapanganbola.models.ApiData;
 import bola.wiradipa.org.lapanganbola.models.Booking;
-import bola.wiradipa.org.lapanganbola.models.City;
 import bola.wiradipa.org.lapanganbola.helpers.adapters.GridViewAdapter;
-import bola.wiradipa.org.lapanganbola.models.Booking;
 import bola.wiradipa.org.lapanganbola.models.Field;
 
 public class DetailFieldActivity extends BaseActivity {
@@ -60,18 +58,16 @@ public class DetailFieldActivity extends BaseActivity {
     private GridViewAdapter gvAdapter;
     private FieldPagerAdapter vpAdapter;
     private LinearLayout sliderDotspanel;
-    private GridView gvSchedule;
 
     private int dotscount;
     private ImageView[] dots;
     private boolean canSubmit=false;
     private NonScrollGridView nonScrollGridView;
+    private RecyclerView recyclerView;
 
     DatePickerDialog.OnDateSetListener mDatePicker;
     String df_full, mDate, name;
 
-    DatePickerDialog.OnDateSetListener mDatePicker;
-    String df_full, mDate, name;
 
     private Booking booking;
     private List<Booking> bookingList;
@@ -99,16 +95,21 @@ public class DetailFieldActivity extends BaseActivity {
         tvBookingCancel = findViewById(R.id.booking_cancel);
         tvTanggal = findViewById(R.id.tanggal);
         btnSubmit = findViewById(R.id.submit);
-        gvSchedule = findViewById(R.id.gridview);
+//        gvSchedule = findViewById(R.id.gridview);
 
         nonScrollGridView = (NonScrollGridView) findViewById(R.id.spotsView);
         nonScrollGridView.setExpanded(true);
-        GridViewAdapter adapter = new GridViewAdapter(this, booking);
-        nonScrollGridView.setAdapter(adapter);
-        adapter.notifyDataSetChanged();
-        canSubmit = getBooleanExtraData(CAN_SUBMIT);
+        recyclerView=findViewById(R.id.gridview);
+
+        gvAdapter = new GridViewAdapter(this, bookingList);
+        recyclerView.setLayoutManager(new GridLayoutManager(this,3));
+        recyclerView.setAdapter(gvAdapter);
+
+        gvAdapter.notifyDataSetChanged();
+            canSubmit = getBooleanExtraData(CAN_SUBMIT);
 //        parsingData();
         Toast.makeText(context, getStringExtraData(FILTER_DATE), Toast.LENGTH_SHORT).show();
+
 
         if(canSubmit) {
             btnSubmit.setVisibility(View.VISIBLE);
@@ -168,18 +169,18 @@ public class DetailFieldActivity extends BaseActivity {
                     Integer.parseInt(getStringExtraData(FILTER_DURATION).split(" ")[0]));
             gvAdapter.setBackgroundEndHour(DateHelper.formatDate("kk:mm", endDate));
         }
-        gvSchedule.setAdapter(gvAdapter);
-        gvSchedule.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @SuppressLint("ResourceType")
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-//                if (gvAdapter.lyBackground.getTag().equals(R.drawable.dot_tersedia)){
-//                    gvAdapter.lyBackground.setBackground(ContextCompat.getDrawable(context, R.drawable.dot_green));
-//                    gvAdapter.lyBackground.setId(R.drawable.dot_green);
-//                }
-                Toast.makeText(context, adapterView.getSelectedItem()+"", Toast.LENGTH_SHORT).show();
-            }
-        });
+        recyclerView.setAdapter(gvAdapter);
+//        gvSchedule.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @SuppressLint("ResourceType")
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+////                if (gvAdapter.lyBackground.getTag().equals(R.drawable.dot_tersedia)){
+////                    gvAdapter.lyBackground.setBackground(ContextCompat.getDrawable(context, R.drawable.dot_green));
+////                    gvAdapter.lyBackground.setId(R.drawable.dot_green);
+////                }
+//                Toast.makeText(context, adapterView.getSelectedItem()+"", Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
         dotscount = vpAdapter.getCount();
         dots = new ImageView[dotscount];
@@ -221,78 +222,78 @@ public class DetailFieldActivity extends BaseActivity {
             }
         });
     }
-    
-    private String getDate(String date){
-        final Calendar cal = Calendar.getInstance();
-        int year = cal.get(Calendar.YEAR);
-        int month = cal.get(Calendar.MONTH);
-        int day = cal.get(Calendar.DAY_OF_MONTH);
-        String tempDate = null;
 
-        Locale local = new Locale("id", "id");
+//    private String getDate(String date){
+//        final Calendar cal = Calendar.getInstance();
+//        int year = cal.get(Calendar.YEAR);
+//        int month = cal.get(Calendar.MONTH);
+//        int day = cal.get(Calendar.DAY_OF_MONTH);
+//        String tempDate = null;
+//
+//        Locale local = new Locale("id", "id");
+//
+//        month = month + 1;
+//        date = year + "-" + checkDigit(month) + "-" + checkDigit(day);
+//
+//        cal.setTimeInMillis(0);
+//        cal.set(year, month, day, 0, 0, 0);
+//
+//        final DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.FULL, local);
+//        df_full = dateFormat.format(cal.getTime());
+//
+//        tempDate = getIntent().getStringExtra("date");
+//        if(tempDate!=null){
+//            date = tempDate;
+//        }else{
+//            date = year + "-" + checkDigit(month) + "-" + checkDigit(day);
+//        }
+//
+//        tvTanggal.setText(df_full);
+//        mDatePicker = new DatePickerDialog.OnDateSetListener() {
+//            @Override
+//            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+//                month = month + 1;
+//                String tempDate = year + "-" + checkDigit(month) + "-" + checkDigit(dayOfMonth);
+//                cal.set(year, month, dayOfMonth, 0 , 0 , 0);
+//                df_full = dateFormat.format(cal.getTime());
+////                if (!getStringExtraData(FILTER_DATE).equals(R.string.hint_rent_date)){
+////                    df_full = getStringExtraData(FILTER_DATE);
+////                } else {
+////                    df_full = dateFormat.format(cal.getTime());
+////                }
+//                tvTanggal.setText(df_full);
+//                gvAdapter.clearData();
+//                mApiService.getScheduleField(getUserToken(), getLongExtraData(EXTRA_ID), tempDate)
+//                        .enqueue(getSchedule.build());
+////                refreshActivity();
+//
+//            }
+//        };
+//
+//        tvTanggal.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Calendar cal = Calendar.getInstance();
+//                int year = cal.get(Calendar.YEAR);
+//                int month = cal.get(Calendar.MONTH);
+//                int day = cal.get(Calendar.DAY_OF_MONTH);
+//
+//                DatePickerDialog dialog = new DatePickerDialog(DetailFieldActivity.this,
+//                        R.style.Theme_AppCompat_DayNight_Dialog_MinWidth,
+//                        mDatePicker,year,month,day);
+//                dialog.getDatePicker().setMinDate(cal.getTimeInMillis());
+//                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+//                dialog.show();
+//            }
+//        });
+//
+//        return date;
+//    }
 
-        month = month + 1;
-        date = year + "-" + checkDigit(month) + "-" + checkDigit(day);
-
-        cal.setTimeInMillis(0);
-        cal.set(year, month, day, 0, 0, 0);
-
-        final DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.FULL, local);
-        df_full = dateFormat.format(cal.getTime());
-
-        tempDate = getIntent().getStringExtra("date");
-        if(tempDate!=null){
-            date = tempDate;
-        }else{
-            date = year + "-" + checkDigit(month) + "-" + checkDigit(day);
-        }
-
-        tvTanggal.setText(df_full);
-        mDatePicker = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                month = month + 1;
-                String tempDate = year + "-" + checkDigit(month) + "-" + checkDigit(dayOfMonth);
-                cal.set(year, month, dayOfMonth, 0 , 0 , 0);
-                df_full = dateFormat.format(cal.getTime());
-//                if (!getStringExtraData(FILTER_DATE).equals(R.string.hint_rent_date)){
-//                    df_full = getStringExtraData(FILTER_DATE);
-//                } else {
-//                    df_full = dateFormat.format(cal.getTime());
-//                }
-                tvTanggal.setText(df_full);
-                gvAdapter.clearData();
-                mApiService.getScheduleField(getUserToken(), getLongExtraData(EXTRA_ID), tempDate)
-                        .enqueue(getSchedule.build());
-//                refreshActivity();
-
-            }
-        };
-
-        tvTanggal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Calendar cal = Calendar.getInstance();
-                int year = cal.get(Calendar.YEAR);
-                int month = cal.get(Calendar.MONTH);
-                int day = cal.get(Calendar.DAY_OF_MONTH);
-
-                DatePickerDialog dialog = new DatePickerDialog(DetailFieldActivity.this,
-                        R.style.Theme_AppCompat_DayNight_Dialog_MinWidth,
-                        mDatePicker,year,month,day);
-                dialog.getDatePicker().setMinDate(cal.getTimeInMillis());
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
-                dialog.show();
-            }
-        });
-
-        return date;
-    }
-    
-    public String checkDigit(int number)
-    {
-        return number<=9?"0"+number:String.valueOf(number);
-    }
+//    public String checkDigit(int number)
+//    {
+//        return number<=9?"0"+number:String.valueOf(number);
+//    }
 
     private String getDate(String date){
         final Calendar cal = Calendar.getInstance();
@@ -392,7 +393,7 @@ public class DetailFieldActivity extends BaseActivity {
             showSnackbar(errorMessage);
         }
     };
-  
+
     ApiCallback getSchedule = new ApiCallback() {
         @Override
         public void onApiSuccess(String result) {
@@ -412,7 +413,7 @@ public class DetailFieldActivity extends BaseActivity {
             showSnackbar(errorMessage);
         }
     };
-  
+
     public void setValue(String name){
         this.name = name;
     }
--- app/src/main/java/bola/wiradipa/org/lapanganbola/helpers/adapters/GridViewAdapter.java
+++ app/src/main/java/bola/wiradipa/org/lapanganbola/helpers/adapters/GridViewAdapter.java
@@ -1,8 +1,10 @@
-package bola.wiradipa.org.lapanganbola;
+package bola.wiradipa.org.lapanganbola.helpers.adapters;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,17 +15,48 @@ import android.widget.Toast;
 
 import java.util.List;
 
+import bola.wiradipa.org.lapanganbola.R;
 import bola.wiradipa.org.lapanganbola.models.Booking;
 
-public class GridViewAdapter extends BaseAdapter {
+public class GridViewAdapter extends RecyclerView.Adapter<GridViewAdapter.GridViewHolder> {
 
     Context mContext;
     List<Booking> listJadwal;
-    private String mStartHour, mEndHour;
+    private String mStartHour,mEndHour;
+
 
     public GridViewAdapter(Context mContext, List<Booking> listJadwal) {
         this.mContext = mContext;
         this.listJadwal = listJadwal;
+    }
+    public class GridViewHolder extends RecyclerView.ViewHolder {
+        TextView tvHour,tvCost;
+        LinearLayout lyBackground;
+
+        public GridViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvHour = (TextView) itemView.findViewById(R.id.jam);
+            tvCost = (TextView) itemView.findViewById(R.id.harga);
+            lyBackground = (LinearLayout) itemView.findViewById(R.id.ly_grid_view);
+        }
+    }
+
+    @NonNull
+    @Override
+    public GridViewAdapter.GridViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.content_grid_view,viewGroup,false);
+        return new GridViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GridViewAdapter.GridViewHolder gridViewHolder, int i) {
+        gridViewHolder.tvHour.setText(getStartHour(i));
+        gridViewHolder.tvCost.setText(listJadwal.get(i).getHarga());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listJadwal.size();
     }
 
     public class ViewHolder{
@@ -31,20 +64,9 @@ public class GridViewAdapter extends BaseAdapter {
         LinearLayout lyBackground;
     }
 
-    @Override
-    public int getCount() {
-        return listJadwal.size();
-    }
 
-    @Override
-    public Object getItem(int i) {
-        return null;
-    }
 
-    @Override
-    public long getItemId(int i) {
-        return 0;
-    }
+
 
     public String getStartHour(int i){
         return listJadwal.get(i).getJam();
@@ -59,83 +81,83 @@ public class GridViewAdapter extends BaseAdapter {
         notifyDataSetChanged();
     }
 
-    @SuppressLint("ResourceType")
-    @Override
-    public View getView(final int i, View view, ViewGroup viewGroup) {
-        ViewHolder holder = null;
-        if (view == null){
-            view = LayoutInflater.from(mContext).inflate(R.layout.content_grid_view, viewGroup, false);
-            holder = new ViewHolder();
-            holder.tvHour = (TextView) view.findViewById(R.id.jam);
-            holder.tvCost = (TextView) view.findViewById(R.id.harga);
-            holder.lyBackground = (LinearLayout) view.findViewById(R.id.ly_grid_view);
+//    @SuppressLint("ResourceType")
+//    @Override
+//    public View getView(final int i, View view, final ViewGroup viewGroup) {
+//        ViewHolder holder = null;
+//        if (view == null){
+//
+//            holder = new ViewHolder();
+//            holder.tvHour = (TextView) view.findViewById(R.id.jam);
+//            holder.tvCost = (TextView) view.findViewById(R.id.harga);
+//            holder.lyBackground = (LinearLayout) view.findViewById(R.id.ly_grid_view);
+//
+//            final Booking bookingList = listJadwal.get(i);
+//            if (bookingList != null){
+//                holder.tvHour.setText(bookingList.getJam()+":00");
+//                if (bookingList.getHarga().equals("")){
+//                    holder.tvCost.setText("0K");
+//                } else {
+//                    holder.tvCost.setText(bookingList.getHarga());
+//                }
+//
+//                if (bookingList.getStatus() == 1){
+//                    holder.lyBackground.setBackground(ContextCompat.getDrawable(mContext,R.drawable.dot_yellow));
+//                    holder.lyBackground.setId(R.drawable.dot_yellow);
+//                } else {
+//                    holder.lyBackground.setBackground(ContextCompat.getDrawable(mContext,R.drawable.dot_tersedia));
+//                    holder.lyBackground.setId(R.drawable.dot_tersedia);
+//
+//                }
+//
+//                Boolean isCheck = false;
+//                if (mStartHour.equals(R.string.hint_hour) || mEndHour == null){
+//                    isCheck = false;
+//                }
+//                else {
+//                    String[] splitStartHour = mStartHour.split(":");
+//                    String[] splitEndHour = mEndHour.split(":");
+//                    int startHour = Integer.parseInt(String.valueOf(splitStartHour[0]));
+//                    int endHour = Integer.parseInt(splitEndHour[0]);
+//                    for (int x=startHour; x<endHour; x++){
+//                        if (String.valueOf(x).equals(bookingList.getJam())){
+//                            holder.lyBackground.setBackground(ContextCompat.getDrawable(mContext,R.drawable.dot_green));
+//                            holder.lyBackground.setId(R.drawable.dot_green);
+//                        }
+//                    }
+//                    isCheck = true;
+//                }
+//            }
+//
+//            final ViewHolder finalHolder = holder;
+//            holder.lyBackground.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    int parseStartHour = Integer.parseInt(getStartHour(i));
+//                    int parseEndHour = Integer.parseInt(getEndHour(i));
+//                    for (int a = parseStartHour; a < parseEndHour; a++) {
+//                        if (finalHolder.lyBackground.getId() == R.drawable.dot_tersedia) {
+//                            int holder = finalHolder.lyBackground.getId();
+//                            finalHolder.lyBackground.setBackground(ContextCompat.getDrawable(mContext, R.drawable.dot_green));
+//                            finalHolder.lyBackground.setId(R.drawable.dot_green);
+//                        } else if (finalHolder.lyBackground.getId() == R.drawable.dot_green) {
+//                            finalHolder.lyBackground.setBackground(ContextCompat.getDrawable(mContext, R.drawable.dot_tersedia));
+//                            finalHolder.lyBackground.setId(R.drawable.dot_tersedia);
+//                        } else if (finalHolder.lyBackground.getId() == R.drawable.dot_yellow) {
+//                            Toast.makeText(mContext, "Sudah dibooking oleh orang lain", Toast.LENGTH_SHORT).show();
+//                        }
+//                    }
+//                }
+//            });
+//        }
+//
+//        return view;
+//    }
 
-            final Booking bookingList = listJadwal.get(i);
-            if (bookingList != null){
-                holder.tvHour.setText(bookingList.getJam()+":00");
-                if (bookingList.getHarga().equals("")){
-                    holder.tvCost.setText("0K");
-                } else {
-                    holder.tvCost.setText(bookingList.getHarga());
-                }
-
-                if (bookingList.getStatus() == 1){
-                    holder.lyBackground.setBackground(ContextCompat.getDrawable(mContext,R.drawable.dot_yellow));
-                    holder.lyBackground.setId(R.drawable.dot_yellow);
-                } else {
-                    holder.lyBackground.setBackground(ContextCompat.getDrawable(mContext,R.drawable.dot_tersedia));
-                    holder.lyBackground.setId(R.drawable.dot_tersedia);
-
-                }
-
-                Boolean isCheck = false;
-                if (mStartHour.equals(R.string.hint_hour) || mEndHour == null){
-                    isCheck = false;
-                }
-                else {
-                    String[] splitStartHour = mStartHour.split(":");
-                    String[] splitEndHour = mEndHour.split(":");
-                    int startHour = Integer.parseInt(String.valueOf(splitStartHour[0]));
-                    int endHour = Integer.parseInt(splitEndHour[0]);
-                    for (int x=startHour; x<endHour; x++){
-                        if (String.valueOf(x).equals(bookingList.getJam())){
-                            holder.lyBackground.setBackground(ContextCompat.getDrawable(mContext,R.drawable.dot_green));
-                            holder.lyBackground.setId(R.drawable.dot_green);
-                        }
-                    }
-                    isCheck = true;
-                }
-            }
-
-            final ViewHolder finalHolder = holder;
-            holder.lyBackground.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    int parseStartHour = Integer.parseInt(getStartHour(i));
-                    int parseEndHour = Integer.parseInt(getEndHour(i));
-                    for (int a=parseStartHour; a<parseEndHour; a++){
-                        if (finalHolder.lyBackground.getId() == R.drawable.dot_tersedia){
-                            finalHolder.lyBackground.setBackground(ContextCompat.getDrawable(mContext,R.drawable.dot_green));
-                            finalHolder.lyBackground.setId(R.drawable.dot_green);
-
-                        } else if (finalHolder.lyBackground.getId() == R.drawable.dot_green) {
-                            finalHolder.lyBackground.setBackground(ContextCompat.getDrawable(mContext,R.drawable.dot_tersedia));
-                            finalHolder.lyBackground.setId(R.drawable.dot_tersedia);
-                        } else if (finalHolder.lyBackground.getId() == R.drawable.dot_yellow){
-                            Toast.makeText(mContext, "Sudah dibooking oleh orang lain", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                }
-            });
-        }
-
-        return view;
-    }
-
-    public String checkDigit(String number) {
-        return Integer.parseInt(number)<=9?"0"+number+":00":number +":00";
-    }
-
+    //    public String checkDigit(String number) {
+//        return Integer.parseInt(number)<=9?"0"+number+":00":number +":00";
+//    }
+//
     public void setBackgroundStartHour(String mStartHour){
         this.mStartHour = mStartHour;
     }
